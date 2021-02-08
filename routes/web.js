12c4b5b41b3792f30d31f762c37aac5a01a488cb
Opilane TTHK <opilane@tthk.ee>
2021-02-08 14:24:37 +0200

    commit 1

---
+++ routes/web.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const contactsController = require('../controllers/contactsController');
+const router = express.Router();
+
+/// Routes
+// home page
+router.get('/', contactsController.getHomePage);
+
+// // post new contact
+router.post('/add-contact', contactsController.postNewContact);
+
+// // delete contact
+router.post('/delete-contact', contactsController.deleteContact);
+
+module.exports = router;
